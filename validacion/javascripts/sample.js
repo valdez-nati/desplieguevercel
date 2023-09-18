932885804bbb66a89c3dc8b35ee3510dff2d7183
valdez-nati <113633115+valdez-nati@users.noreply.github.com>
2023-09-18 13:57:46 -0400

    Delete validacion directory

--- validacion/javascripts/sample.js
+++
@@ -1,8 +0,0 @@
-function clickBtn() {
-    const button = document.getElementById("btn");
-    button.addEventListener("click", function() {
-      alert("Pulsado.");
-    });
-  };
-  
-  window.onload = clickBtn;
